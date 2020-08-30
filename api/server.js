76657203fdcdb0ba2191cfc9f017ac6e39d9d402
Gading <sutan.gnst@gmail.com>
2020-08-30 13:34:24 +0700

    set header cache control

--- api/server.js
+++ api/server.js
@@ -6,7 +6,14 @@ const port = process.env.PORT || 3000
 const server = express()
 
 server.use(cors())
+
+server.use((_, res, next) => {
+    res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate')
+    next()
+})
+
 server.use(express.json())
+
 server.use(routes)
 
 server.listen(port, () => {
