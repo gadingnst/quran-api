4c513ad7b985bf347e546471f49d465b1b0d747a
Gading Nasution <sutan.gnst@gmail.com>
2022-06-11 16:17:57 +0700

    improvement: limiter

--- api/routes.js
+++ api/routes.js
@@ -1,19 +1,13 @@
 const { Router } = require('express')
+
+const limiter = require('./limiter')
 const { caching } = require('./middlewares')
 const SurahHandler = require('./handlers/surah')
 const JuzHandler = require('./handlers/juzHandler')
 
 const router = Router()
 
-// surah router
-router.get('/surah', caching, SurahHandler.getAllSurah)
-router.get('/surah/:surah', caching, SurahHandler.getSurah)
-router.get('/surah/:surah/:ayah', caching, SurahHandler.getAyahFromSurah)
-
-// juz router
-router.get('/juz/:juz', caching, JuzHandler.getJuz)
-
-// root router
+/* whitelist rate-limit */
 router.get('/', (req, res) => res.status(200).send({
     surah: {
         listSurah: '/surah',
@@ -29,6 +23,19 @@ router.get('/', (req, res) => res.status(200).send({
     maintaner: 'Sutan Gading Fadhillah Nasution <contact@gading.dev>',
     source: 'https://github.com/gadingnst/quran-api'
 }))
+
+router.get('/surah', caching, SurahHandler.getAllSurah)
+/* end whitelist rate-limit */
+
+// rate limiter middleware
+router.use(limiter)
+
+// surah router
+router.get('/surah/:surah', caching, SurahHandler.getSurah)
+router.get('/surah/:surah/:ayah', caching, SurahHandler.getAyahFromSurah)
+
+// juz router
+router.get('/juz/:juz', caching, JuzHandler.getJuz)
 
 // fallback router
 router.all('*', (req, res) => res.status(404).send({
--- api/server.js
+++ api/server.js
@@ -2,64 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
-const rateLimit = require('express-rate-limit')
-const MongoStore = require('rate-limit-mongo')
 const routes = require('./routes')
-const { MONGODB_URI } = require('../config')
 
 const port = process.env.PORT || 3000
 const server = express()
 
 server.set('trust proxy', 1)
-
 server.use(cors())
-
-/**
- * Only on my production server
- * you can probably remove this code
- * if your server won't to have rate limitter API usage
- * in my case, I'll limit it with 10req/30min/address & 120req/30min for the whole server
- * @see https://www.npmjs.com/package/rate-limit-mongo
- */
-if (MONGODB_URI) {
-    const store = new MongoStore({
-        uri: MONGODB_URI,
-        expireTimeMs: 1000 * 60 * 30,
-        errorHandler: console.error.bind(null, 'rate-limit-mongo'),
-        collectionName: 'request-records'
-    })
-    server.use(rateLimit({
-        store,
-        max: 120,
-        windowMs: 1000 * 60 * 30,
-        keyGenerator: () => '__global',
-        message: {
-            code: 429,
-            status: 'Too Many Requests',
-            message: 'The server has too received many requests incoming. Try again in a few minutes.',
-            data: {}
-        }
-    }))
-    server.use(rateLimit({
-        store,
-        max: 10,
-        windowMs: 1000 * 60 * 30,
-        message: {
-            code: 429,
-            status: 'Too Many Requests',
-            message: 'You have exceeded the rate limit. Try again in a few minutes.',
-            data: {}
-        }
-    }))
-} else {
-    server.use((req, res, next) => {
-        res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate')
-        next()
-    })
-}
-
 server.use(express.json())
-
 server.use(routes)
 
 server.listen(port, () => {
